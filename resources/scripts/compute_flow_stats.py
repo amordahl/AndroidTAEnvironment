90db04347dfc8be5b118bd07b0fec3521f6d754a
Austin Mordahl <austin.mordahL@utdallas.edu>
2020-05-19 16:17:44 +0000

    updated some configuration files, scripts, and flowdroid results

--- resources/scripts/compute_flow_stats.py
+++ resources/scripts/compute_flow_stats.py
@@ -4,12 +4,15 @@ import xml.etree.ElementTree as ET
 p = argparse.ArgumentParser('compute statistics for flows')
 
 p.add_argument('classifications')
-p.add_argument('results', nargs='*')
+p.add_argument('results_file')
 
 args = p.parse_args()
+with open(args.results_file) as f:
+    results = f.readlines()
 
+results = [r.strip() for r in results]
 # First, find all of the true flows
-root = ET.parse(p.classifications).getroot()
+root = ET.parse(args.classifications).getroot()
 
 
 def is_true(f):
@@ -28,8 +31,12 @@ for t in trues:
 
 print(header)
 
-for r in args.results:
-    flows = [Flow(f) for f in ET.parse(r).getroot()]
+for r in results:
+    root = ET.parse(r).getroot()
+    try:
+        flows = [Flow(f) for f in ET.parse(r).getroot()[0]]
+    except IndexError:
+        flows = []
     row = f'{r}'
     for t in trues:
         result = 0 if t not in flows else 1

